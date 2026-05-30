b04a2aaf8da5b740b3c42165cdf689a126e376ff
ashikapoojary <ashika.poojary.a@gmail.com>
2026-05-31 00:00:22 +0530

    Update crime_dashboard.py

--- crime_dashboard.py
+++ crime_dashboard.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
@@ -461,4 +462,6 @@ def drill_down(click_data, close_clicks, city, age_group, genders):
 
 if __name__ == '__main__':
     print('\n🚀  Dashboard starting → http://127.0.0.1:8050\n')
-    app.run(debug=False, port=8050, host='0.0.0.0')
+   
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=False, host="0.0.0.0", port=port)
